4245a80b324ce6d3af40b1b8b9b31f889a264218
mira <mira.eschliman@outlook.com>
2017-11-29 19:02:43 -0600

    no message

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by mira.eschliman on 11/6/2017.
+ */
+{
+//start camera
+        public.camera.power_on;
+//start motor & scan glyph
+        public.camera.powower on;
+        public.glyph.getglyph();
+ // move forward
+        public.drive.forward();
+ // turn of face blocks
+        public.drive.turn();
+ // move forward
+        public.drive.forward;
+ //grip block
+        public.lifter.open claw();
+        public. lifter.close claw;
+ // move forward
+        public.drive.turn();
+        public.drive.forward;
+ // place block
+        public.lifter.open claw;
+ // repeat for 30 seconds
+        public
+ //power motors to 0
+        public.set motors(0)
+
+
+
+
