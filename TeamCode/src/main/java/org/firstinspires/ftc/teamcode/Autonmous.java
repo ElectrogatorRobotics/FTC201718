b5228042db0ad73a91211d9b83cb677ff31862ae
mira <mira.eschliman@outlook.com>
2017-11-29 19:39:33 -0600

    Added class header

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous.java
@@ -1,35 +1,49 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 /**
  * Created by mira.eschliman on 11/6/2017.
  */
-{
+@TeleOp(name = "Autonmous")
+public class Autonmous extends LinearOpMode {
+
+        @Override
+        public void runOpMode() throws InterruptedException {
 //start camera
-        public.camera.power_on;
+                public.camera.power_on;
 //start motor & scan glyph
-        public.camera.powower on;
-        public.glyph.getglyph();
- // move forward
-        public.drive.forward();
- // turn of face blocks
-        public.drive.turn();
- // move forward
-        public.drive.forward;
- //grip block
-        public.lifter.open claw();
-        public. lifter.close claw;
- // move forward
-        public.drive.turn();
-        public.drive.forward;
- // place block
-        public.lifter.open claw;
- // repeat for 30 seconds
-        public
- //power motors to 0
-        public.set motors(0)
+                public.camera.powower_on;
+                public.glyph.getglyph();
+                // move forward
+                public.drive.forward();
+                // turn of face blocks
+                public.drive.turn();
+                // move forward
+                public.drive.forward;
+                //grip block
 
+                public.
 
+                lifter.open claw();
+
+                public.
+                lifter.close claw;
+                // move forward
+                public.drive.turn();
+                public.drive.forward;
+                // place block
+                public.
+                lifter.open claw;
+                // repeat for 30 seconds
+                public.long.30_sececons;
+                //power motors to 0
+                public.
+
+                set motors(0);
+
+        }
+}
 
 
