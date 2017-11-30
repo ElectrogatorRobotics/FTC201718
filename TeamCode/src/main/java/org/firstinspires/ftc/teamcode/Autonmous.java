0efa69c8713c2752c1f7a9f0e8e70e6a96c99b16
mira <mira.eschliman@outlook.com>
2017-11-29 19:58:42 -0600

    started to clean up atonomous mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous.java
@@ -3,6 +3,15 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.library.Camera;
+import org.firstinspires.ftc.teamcode.library.CameraImpl;
+import org.firstinspires.ftc.teamcode.library.Claw;
+import org.firstinspires.ftc.teamcode.library.ClawImpl;
+import org.firstinspires.ftc.teamcode.library.Drive;
+import org.firstinspires.ftc.teamcode.library.DriveImpl;
+import org.firstinspires.ftc.teamcode.library.Lifter;
+import org.firstinspires.ftc.teamcode.library.LifterImpl;
+
 /**
  * Created by mira.eschliman on 11/6/2017.
  */
@@ -11,35 +20,41 @@ public class Autonmous extends LinearOpMode {
 
         @Override
         public void runOpMode() throws InterruptedException {
-//start camera
-                public.camera.power_on;
+            //make our helpers
+            Camera camera = new CameraImpl();
+            Drive drive = new DriveImpl();
+            Lifter lifter= new LifterImpl();
+            Camera.Glyph glyph;
+            Claw claw = new ClawImpl();
+
+
 //start motor & scan glyph
-                public.camera.powower_on;
-                public.glyph.getglyph();
+                glyph = camera.getGlyph();
+
                 // move forward
-                public.drive.forward();
+                drive.forward();
                 // turn of face blocks
-                public.drive.turn();
+                drive.turn();
                 // move forward
-                public.drive.forward;
+                drive.forward;
                 //grip block
 
-                public.
+
 
                 lifter.open claw();
 
-                public.
+
                 lifter.close claw;
                 // move forward
-                public.drive.turn();
-                public.drive.forward;
+                drive.turn();
+                drive.forward;
                 // place block
-                public.
+
                 lifter.open claw;
                 // repeat for 30 seconds
-                public.long.30_sececons;
+                long.30_sececons;
                 //power motors to 0
-                public.
+
 
                 set motors(0);
 
